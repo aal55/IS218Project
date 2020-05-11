59922fbbbde0d6dbc2b238363a74106841745a30
DylanCGreen <dcg24@njit.edu>
2020-05-10 23:42:53 -0400

    Fix: Removed most of hwValidation code

--- js/hwValidation.js
+++ js/hwValidation.js
@@ -1,23 +1,10 @@
 function hwValidation() {
     var pass = true;
-    var dateformat = /^(0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])[\/\-]\d{4}$/;
     var message = "Warning: ";
-    var date = document.getElementById("date");
-    var title = document.getElementById("title");
 	var description = document.getElementById("description");
-	
-	if(date.value === "") {
-        message += "\nPlease enter a date.";
-        date.style.borderColor = "red";
-        pass = false;
-    }
-	if(title.value === "") {
-        message += "\nPlease enter a title.";
-        date.style.borderColor = "red";
-        pass = false;
-    }
-	if(description.value === "" || description.length > 144) {
-        message += "\nPlease enter a description no longer than 144 characters.";
+
+	if(description.length > 144) {
+        message += "\nMust be less than 144 characters.";
         date.style.borderColor = "red";
         pass = false;
     }
