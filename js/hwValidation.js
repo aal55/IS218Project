de265f17c0a405504c4e68c16f6a319c4c9c00d1
DylanCGreen <dcg24@njit.edu>
2020-05-10 23:17:19 -0400

    Fix: Fixed hwValidation date value

--- js/hwValidation.js
+++ js/hwValidation.js
@@ -6,46 +6,11 @@ function hwValidation() {
     var title = document.getElementById("title");
 	var description = document.getElementById("description");
 	
-	if (date.value.match(dateformat)) {
-		document.form1.text1.focus();
-		var opera1 = inputText.value.split('/');
-		var opera2 = inputText.value.split('-');
-		lopera1 = opera1.length;
-		lopera2 = opera2.length;
-		if (lopera1>1) {
-			var pdate = inputText.value.split('/');
-		} else if (lopera2>1) {
-			var pdate = inputText.value.split('-');
-		}
-		var mm  = parseInt(pdate[0]);
-		var dd = parseInt(pdate[1]);
-		var yy = parseInt(pdate[2]);
-		var ListofDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-		if (mm==1 || mm>2) {
-			if (dd>ListofDays[mm-1]) {
-				alert('Invalid date format!');
-				pass = false;
-			}
-		}
-		if (mm==2) {
-			var lyear = false;
-			if ( (!(yy % 4) && yy % 100) || !(yy % 400))  {
-				lyear = true;
-			}
-			if ((lyear==false) && (dd>=29)) {
-				alert('Invalid date format!');
-				pass = false;
-			}
-			if ((lyear==true) && (dd>29)) {
-				alert('Invalid date format!');
-				pass = false;
-			}
-		}
-	} else {
-		alert("Invalid date format!");
-		document.form1.text1.focus();
-		pass = false;
-	}
+	if(date.value === "") {
+        message += "\nPlease enter a date.";
+        date.style.borderColor = "red";
+        pass = false;
+    }
 	if(title.value === "") {
         message += "\nPlease enter a title.";
         date.style.borderColor = "red";
